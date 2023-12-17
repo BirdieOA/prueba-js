71ce8a07015f7002866e4023cc1c3f8d72d3bbd3
BirdieOA <maitedeleonf@gmail.com>
2023-12-17 11:15:08 -0300

    final

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let select = document.querySelector("select");
 let cambio;
+let tipoCambio;
+let donacion=0;
 
 const CAMBIOS = [
   { moneda: "Dólares USA", elCambio: 1.06 },
   { moneda: "Libras esterlinas", elCambio: 0.87 },
-  { moneda: "Pesos argentinos", elCambio: 370.22 },
+  { moneda: "Pesos argentinos", elCambio: 863.22 },
   { moneda: "Pesos colombianos", elCambio: 4510.51 },
   { moneda: "Pesos mexicanos", elCambio: 19.33 }
 ];
@@ -17,33 +19,62 @@ CAMBIOS.map((valor) =>
 );
 
 modificarCambio();
-select.addEventListener("change", modificarCambio);
+actualizarDonaciones();
+document.querySelector("#euros").addEventListener("keyup", cambio1);
+document.querySelector("#divisa").addEventListener("keyup", cambio2);
+select.addEventListener("change", recalcular);
+document.querySelector("#donar").addEventListener("click", donar)
 
-function modificarCambio() {
-  cambio=select.value;
-  document.querySelector("#cambio").innerHTML = cambio;
-  const INDICE= select.selectedIndex;
-  const MONEDA= select[INDICE].text;
+
+function donar(){
+ donacion=decimales(donacion+decimales(Number(document.querySelector("#euros").value)*0.05));
+ actualizarDonaciones();
+ limpiarInput();
+}
+
+function actualizarDonaciones(){
+  document.querySelector("#donacion").value=donacion;
 }
 
 
+function recalcular(){
+  modificarCambio();
+  (tipoCambio===1)
+  ? cambio1()
+  : cambio2()
 
+}
 
+function cambio1() {
+  tipoCambio = 1;
+  let escrito = Number(document.querySelector("#euros").value);
+  let resultado = decimales(escrito * cambio);
+  document.querySelector("#divisa").value = resultado;
+}
 
+function cambio2() {
+  tipoCambio = 2;
+  let escrito = Number(document.querySelector("#divisa").value);
+  let resultado = decimales(escrito / cambio);
+  document.querySelector("#euros").value = resultado;
+}
 
+function decimales(cantidad) {
+  return Number(cantidad.toFixed(2));
+}
 
+function modificarCambio() {
+  cambio = select.value;
+  const INDICE = select.selectedIndex;
+  const MONEDA = select[INDICE].text.toLowerCase();
+  document.querySelector("#cambio").innerHTML = `1€ es ${cambio} ${MONEDA}`;
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function limpiarInput(){
+  document.querySelector("#euros").value="";
+  document.querySelector("#divisa").value = "";
+  document.querySelector("#euros").focus();
+}
 
 // select.insertAdjacentHTML(
 //   "beforeend",
